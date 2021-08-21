5814b924d55b8dc4b4ed8c2af5cb0b638c89ac6a
mebius <mebius1080p@gmail.com>
2021-08-21 21:32:43 +0900

    first commit

---
+++ RandomData/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace RandomData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mb = args[0];
+            int numMB = 0;
+            bool isNumConversionSuccess = int.TryParse(mb, out numMB);
+            if (!isNumConversionSuccess)
+            {
+                Console.WriteLine("enter number arg");
+                return;
+            }
+            int byteSize = numMB * 1024 * 1024;
+            byte[] bytes = new byte[byteSize];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetNonZeroBytes(bytes);
+
+            using (FileStream fs = new FileStream("random.data", FileMode.Create))
+            {
+                fs.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
